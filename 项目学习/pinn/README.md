948510fd7825bead85a35351c7d2b5f40e9d5203
period <yliu60755@gmail.com>
2024-11-11 15:14:36 +0800

    重新添加

---
+++ 项目学习/pinn/README.md
@@ -0,0 +1,77 @@
+[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.3356876.svg)](https://doi.org/10.5281/zenodo.3356876)
+[![PyPI version](https://badge.fury.io/py/pml-pinn.svg)](https://badge.fury.io/py/pml-pinn)
+# Physics-informed neural networks package
+Welcome to the PML repository for physics-informed neural networks. We will use this repository to disseminate our research in this exciting topic.
+
+## Install
+
+To install the stable version just do:
+```
+pip install pml-pinn
+```
+
+### Develop mode
+
+To install in develop mode, clone this repository and do a pip install:
+```
+git clone https://github.com/PML-UCF/pinn.git
+cd pinn
+pip install -e .
+```
+
+## Citing this repository
+
+Please, cite this repository using: 
+
+    @misc{2019_pinn,
+        author    = {Felipe A. C. Viana and Renato G. Nascimento and Yigit Yucesan and Arinan Dourado},
+        title     = {Physics-informed neural networks package},
+        month     = Aug,
+        year      = 2019,
+        doi       = {10.5281/zenodo.3356876},
+        version   = {0.0.3},
+        publisher = {Zenodo},
+        url       = {https://github.com/PML-UCF/pinn}
+        }
+The corresponding reference entry should look like:
+
+F. A. C. Viana, R. G. Nascimento, Y. Yucesan, and A. Dourado, Physics-informed neural networks package, v0.0.3, Aug. 2019. doi:10.5281/zenodo.3356876, URL https://github.com/PML-UCF/pinn.
+
+## Publications
+
+Over time, the following publications out of the PML-UCF research group used/referred to this repository:
+
+### Journal papers
+- A. Dourado, F. A. C. Viana, "[Ensemble of hybrid neural networks to compensate for epistemic uncertainties: a case study in system prognosis](https://link.springer.com/article/10.1007/s00500-022-07129-1)," Soft Computing, Vol. 26 (13), pp. 6157-6173, 2022. (DOI: 10.1007/s00500-022-07129-1).
+
+- Y. A. Yucesan and F. A. C. Viana, "[A hybrid physics-informed neural network for main bearing fatigue prognosis under grease quality variation](https://www.sciencedirect.com/science/article/pii/S088832702200070X)," Mechanical Systems and Signal Processing, Vol. 171, pp. 108875, 2022. (DOI: 10.1016/j.ymssp.2022.108875).
+
+- Y. A. Yucesan, A. Dourado, and F. A. C. Viana, "[A survey of modeling for prognosis and health management of industrial equipment](https://www.sciencedirect.com/science/article/pii/S1474034621001567)," Advanced Engineering Informatics, Vol. 50, pp. 101404, 2021. (DOI: 10.1016/j.aei.2021.101404). 
+
+- F. A. C. Viana and A. K. Subramaniyan, "[A survey of Bayesian calibration and physics-informed neural networks in scientific modeling](https://link.springer.com/article/10.1007/s11831-021-09539-0)," Archives of Computational Methods in Engineering, Vol. 28 (5), pp. 3801-3830, 2021. (DOI: 10.1007/s11831-021-09539-0). . 
+
+- Y. A. Yucesan and F. A. C. Viana, "[Hybrid physics-informed neural networks for main bearing fatigue prognosis with visual grease inspection](https://www.sciencedirect.com/science/article/pii/S0166361520306205)," Computers in Industry, Computers in Industry, Vol. 125, pp. 103386, 2021. (DOI: 10.1016/j.compind.2020.103386).
+
+- F. A. C. Viana, R. G. Nascimento, A. Dourado, and Y. A. Yucesan, "[Estimating model inadequacy in ordinary differential equations with physics-informed neural networks](https://www.sciencedirect.com/science/article/pii/S0045794920302613)," Computers and Structures, Vol. 245, pp. 106458, 2021. (DOI: 10.1016/j.compstruc.2020.106458).
+
+- R. G. Nascimento and F. A. C. Viana, "[Cumulative damage modeling with recurrent neural networks](https://arc.aiaa.org/doi/full/10.2514/1.J059250)," AIAA Journal, Online First, 13 pages, 2020. (DOI: 10.2514/1.J059250).
+
+- A. Dourado and F. A. C. Viana, "[Physics-informed neural networks for missing physics estimation in cumulative damage models: a case study in corrosion fatigue](https://asmedigitalcollection.asme.org/computingengineering/article-abstract/doi/10.1115/1.4047173/1083614/Physics-informed-neural-networks-for-missing)," ASME Journal of Computing and Information Science in Engineering, Vol. 20 (6), 10 pages, 2020. (DOI: 10.1115/1.4047173).
+
+- Y. A. Yucesan and F. A. C. Viana, "[A physics-informed neural network for wind turbine main bearing fatigue](http://www.phmsociety.org/node/2736)," International Journal of Prognostics and Health Management, Vol. 11 (1), 2020. (ISSN: 2153-2648).
+
+
+### Conference papers
+- Y. A. Yucesan and F. A. C. Viana, "[A probabilistic hybrid model for main bearing fatigue considering uncertainty in grease quality](https://arc.aiaa.org/doi/abs/10.2514/6.2021-1243)," AIAA Scitech 2021 Forum, Virtual Event, January 11-15 and 19-21, 2021, AIAA–2021–1243 (DOI: 10.2514/6.2021-1243). 
+
+- Y. A. Yucesan and F. A. C. Viana, "[A hybrid model for wind turbine main bearing fatigue with uncertainty in grease observations](https://phmpapers.org/index.php/phmconf/article/view/1139)," Proceedings of the Annual Conference of the PHM Society, Vol. 12 (1), Virtual Event, November 9-13, 2020 (DOI: 10.36001/phmconf.2020.v12i1.1139). 
+
+- Y. A. Yucesan and F. A. C. Viana, "[A hybrid model for main bearing fatigue prognosis based on physics and machine learning](https://arc.aiaa.org/doi/abs/10.2514/6.2020-1412)," AIAA SciTech Forum, Orlando, USA, January 6-10, 2020, AIAA 2020-1412 (DOI: 10.2514/6.2020-1412). 
+
+- A. Dourado and F. A. C. Viana, "[Physics-informed neural networks for bias compensation in corrosion-fatigue](https://arc.aiaa.org/doi/abs/10.2514/6.2020-1149)," AIAA SciTech Forum, Orlando, USA, January 6-10, 2020, AIAA 2020-1149 (DOI: 10.2514/6.2020-1149).
+
+- A. Dourado and F. A. C. Viana, "[Physics-Informed Neural Networks for Corrosion-Fatigue Prognosis](http://phmpapers.org/index.php/phmconf/article/view/814)," Proceedings of the Annual Conference of the PHM Society, Scottsdale,USA, September 21-26, 2019.
+
+- Y. A. Yucesan and F. A. C. Viana, "[Wind turbine main bearing fatigue life estimation with physics-informed neural networks](http://phmpapers.org/index.php/phmconf/article/view/807)," Proceedings of the Annual Conference of the PHM Society, Vol. 11 (1), Scottsdale, USA, September 21-26, 2019 (DOI:10.36001/phmconf.2019.v11i1.807).
+
+- R.G. Nascimento and F. A. C. Viana, "[Fleet prognosis with physics-informed recurrent neural networks](http://www.dpi-proceedings.com/index.php/shm2019/article/view/32301)," The 12th International Workshop on Structural Health Monitoring, Stanford, USA, September 10-12, 2019 (DOI:10.12783/shm2019/32301).
---
+++ 项目学习/pinn/pinn/layers/core.py
@@ -0,0 +1,177 @@
+# ______          _           _     _ _ _     _   _      
+# | ___ \        | |         | |   (_) (_)   | | (_)     
+# | |_/ / __ ___ | |__   __ _| |__  _| |_ ___| |_ _  ___ 
+# |  __/ '__/ _ \| '_ \ / _` | '_ \| | | / __| __| |/ __|
+# | |  | | | (_) | |_) | (_| | |_) | | | \__ \ |_| | (__ 
+# \_|  |_|  \___/|_.__/ \__,_|_.__/|_|_|_|___/\__|_|\___|
+# ___  ___          _                 _                  
+# |  \/  |         | |               (_)                 
+# | .  . | ___  ___| |__   __ _ _ __  _  ___ ___         
+# | |\/| |/ _ \/ __| '_ \ / _` | '_ \| |/ __/ __|        
+# | |  | |  __/ (__| | | | (_| | | | | | (__\__ \        
+# \_|  |_/\___|\___|_| |_|\__,_|_| |_|_|\___|___/        
+#  _           _                     _                   
+# | |         | |                   | |                  
+# | |     __ _| |__   ___  _ __ __ _| |_ ___  _ __ _   _ 
+# | |    / _` | '_ \ / _ \| '__/ _` | __/ _ \| '__| | | |
+# | |___| (_| | |_) | (_) | | | (_| | || (_) | |  | |_| |
+# \_____/\__,_|_.__/ \___/|_|  \__,_|\__\___/|_|   \__, |
+#                                                   __/ |
+#                                                  |___/ 
+#														  
+# MIT License
+# 
+# Copyright (c) 2019 Probabilistic Mechanics Laboratory
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+""" Core PINN layers
+"""
+from tensorflow.python.keras.layers import Dense
+
+from tensorflow.python.keras import initializers
+from tensorflow.python.keras import regularizers
+from tensorflow.python.keras import constraints
+
+from tensorflow.python.keras.engine.base_layer import Layer
+
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.framework import ops
+
+from tensorflow.python.ops import array_ops
+
+from pinn.layers import interpolate
+
+from tensorflow import shape, expand_dims, constant, cast
+
+import numpy as np
+
+
+def getScalingDenseLayer(input_location, input_scale):
+    recip_input_scale = np.reciprocal(input_scale)
+    
+    waux = np.diag(recip_input_scale)
+    baux = -input_location*recip_input_scale
+    
+    dL = Dense(input_location.shape[0], activation = None, input_shape = input_location.shape)
+    dL.build(input_shape = input_location.shape)
+    dL.set_weights([waux, baux])
+    dL.trainable = False
+    return dL
+
+
+def inputsSelection(inputs_shape, ndex):
+    if not hasattr(ndex,'index'):
+        ndex = list(ndex)
+    input_mask = np.zeros([inputs_shape[-1], len(ndex)])
+    for i in range(inputs_shape[-1]):
+        for v in ndex:
+            if i == v:
+                input_mask[i,ndex.index(v)] = 1
+
+    dL = Dense(len(ndex), activation = None, input_shape = inputs_shape,
+               use_bias = False)
+    dL.build(input_shape = inputs_shape)
+    dL.set_weights([input_mask])
+    dL.trainable = False
+    return dL
+
+
+# class InputsSelection(Layer):
+#     def __init__(self, input_shape, ndex):
+#         super(InputsSelection, self).__init__()
+#
+#         if not hasattr(ndex, 'index'):
+#             ndex = list(ndex)
+#
+#         # 创建输入掩码
+#         self.mask_array = np.zeros([input_shape[-1], len(ndex)])
+#         for i in range(input_shape[-1]):
+#             for v in ndex:
+#                 if i == v:
+#                     self.mask_array[i, ndex.index(v)] = 1
+#
+#         # 使用 Keras 的 Dense 层
+#         self.dense_layer = Dense(len(ndex), activation=None, use_bias=False)
+#
+#     def build(self, input_shape):
+#         # 这里我们确保 Dense 层被正确构建
+#         self.dense_layer.build(input_shape)
+#         self.dense_layer.set_weights([self.mask_array])  # 设置权重
+#         self.built = True  # 标记层为已构建
+#
+#     def call(self, inputs):
+#         return self.dense_layer(inputs)  # 返回计算结果
+
+class TableInterpolation(Layer):
+    """ Table lookup and interpolation implementation.
+        Interrogates provided query points using provided table and outputs the interpolation result.
+        Remarks on this class:
+            - Only supports 2-D tables (f(x1,x2) = y)
+            - If a 1-D table is to be used, it needs to be converted to a 2-D table (see file /samples/core/table_lookup/run01_table_lookup_sample.py)
+            - Extrapolation is not supported (provide a table grid large enough for your case)
+            - Class returns limit values in case of extrapolation attempt.
+            - Provided tables should be equally spaced.
+    """
+    def __init__(self,
+                 kernel_initializer = 'glorot_uniform',
+                 kernel_regularizer=None,
+                 kernel_constraint=None,
+                 table_shape=(1,4,4,1),
+                 **kwargs):
+        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
+        super(TableInterpolation, self).__init__(**kwargs)
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.kernel_constraint  = constraints.get(kernel_constraint)
+        
+        self.table_shape = table_shape
+        
+    def build(self, input_shape, **kwargs):
+        self.grid = self.add_weight("grid",
+                                      shape = self.table_shape,
+                                      initializer = self.kernel_initializer,
+                                      dtype = self.dtype,
+                                      trainable = self.trainable,
+                                      **kwargs)
+        self.bounds = self.add_weight("bounds",
+                                      shape = [2,2],
+                                      initializer = self.kernel_initializer,
+                                      dtype = self.dtype,
+                                      trainable = self.trainable,
+                                      **kwargs)
+        self.built = True
+
+    def call(self, inputs):
+        self.grid = ops.convert_to_tensor(self.grid, dtype=self.dtype)
+        self.bounds = ops.convert_to_tensor(self.bounds,dtype=self.dtype)
+        inputs = ops.convert_to_tensor(inputs, dtype=self.dtype)
+        queryPoints_ind = ((cast(shape(self.grid)[1:3], dtype=self.dtype))-constant(1.0))*(inputs-self.bounds[0])/(self.bounds[1]-self.bounds[0])
+        if inputs.shape.rank == 2:
+            queryPoints_ind = expand_dims(queryPoints_ind,0)
+        output = interpolate(self.grid, queryPoints_ind)
+        if inputs.shape.rank == 2:
+            output = array_ops.reshape(output,(array_ops.shape(output)[1],) + (array_ops.shape(output)[2],))
+        return output
+
+    def compute_output_shape(self, input_shape):
+        aux_shape = tensor_shape.TensorShape((None,1))
+        
+        return aux_shape[:-1].concatenate(1)
